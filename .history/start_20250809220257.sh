e19bab2bf58181b160b46dc1e55ad2782905488a
Pradunya2000 <pradunyasarode@gmail.com>
2025-08-11 19:08:48 +0530

    Remove files from remote repo but keep locally

--- .history/start_20250809220257.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run FastAPI in background on port 8000
-uvicorn api_app:app --host 0.0.0.0 --port 8000 &
-
-# Run Streamlit on port 8501
-streamlit run streamlit/app.py --server.port 8501 --server.address 0.0.0.0
--- test/debug_metadata.py
+++
@@ -1,7 +0,0 @@
-# debug_metadata.py
-from retriever import get_all_metadata
-
-if __name__ == "__main__":
-    all_meta = get_all_metadata()
-    for m in all_meta:
-        print(f"File in DB: '{m['file']}' | Upload Date: {m['upload_date']}'")
